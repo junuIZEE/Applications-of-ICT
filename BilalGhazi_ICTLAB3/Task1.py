f6a375729702ba0469008fa6115c47e62fde0352
BilalGh0zi <bilaltheghazi@gmail.com>
2025-09-22 21:18:45 +0500

    Delete BilalGhazi_ICTLAB3 directory

--- BilalGhazi_ICTLAB3/Task1.py
+++
@@ -1,9 +0,0 @@
-name =str (input("input your name\n"))
-age = int (input("input your age\n"))
-height =float (input("input your height\n"))
-is_student = True #This is a comment
-
-print (name)
-print(age)
-print(height)
-print(is_student)
